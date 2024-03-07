0976063f7b847413237e4811011c50e5d236a382
Miguel Monte e Freitas <miguelmontefreitas@tecnico.ulisboa.pt>
2024-03-07 12:03:04 +0000

    feat(Enrolment): implement service get enrolment list

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrolment/repository/EnrolmentRepository.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrolment/repository/EnrolmentRepository.java
@@ -1,13 +1,19 @@
 package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrolment.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrolment.domain.Enrolment;
 
 @Repository
 @Transactional
 public interface EnrolmentRepository extends JpaRepository<Enrolment, Integer> {
+    @Query("SELECT e FROM Enrolment e WHERE e.activity.id = :activityId")
+    List<Enrolment> getEnrolmentsByActivityId(Integer activityId);
 }
