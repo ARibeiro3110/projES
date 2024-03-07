b71f318e0aa41052eb46024b53fa38a00950f7c5
Miguel Almeida <miguel.pessanha.almeida@tecnico.ulisboa.pt>
2024-03-07 09:55:55 +0000

    fix: parse ErrorMessage file in job pipeline

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrolment/EnrolmentController.java
@@ -0,0 +1,20 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrolment;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto.ActivityDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequestMapping("/enrolments")
+public class EnrolmentController {
+    //
+}
