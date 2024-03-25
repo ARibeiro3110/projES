0318bcd2cdee44dcc37af453a87bd660c1f99a24
luispp018 <luisppereira@tecnico.ulisboa.pt>
2024-03-25 01:02:58 +0000

    feat: Create cypress function to load database Refs: #99

--- frontend/tests/e2e/support/database.js
+++ frontend/tests/e2e/support/database.js
@@ -9,6 +9,9 @@ const credentials = {
 const INSTITUTION_COLUMNS = "institutions (id, active, confirmation_token, creation_date, email, name, nif, token_generation_date)";
 const USER_COLUMNS = "users (user_type, id, creation_date, name, role, state, institution_id)";
 const AUTH_USERS_COLUMNS = "auth_users (auth_type, id, active, email, username, user_id)";
+const ACTIVITY_COLUMNS = "activities (id, application_deadline, creation_date, description, ending_date, name, participants_number_limit, region, starting_date, state, institution_id)";
+const ENROLLMENT_COLUMNS = "enrollments (id, enrollment_date_time, motivation, activity_id, volunteer_id)";
+const PARTICIPATION_COLUMNS = "participations (id, acceptance_date, rating, activity_id, volunteer_id)";
 
 const now = new Date();
 const tomorrow = new Date(now);
@@ -19,6 +22,28 @@ const yesterday = new Date(now);
 yesterday.setDate(now.getDate() - 1);
 const dayBeforeYesterday = new Date(now);
 dayBeforeYesterday.setDate(now.getDate() - 2);
+
+const activityQueries = [
+  "INSERT_INTO" + ACTIVITY_COLUMNS + generateAssessmentActivityTuple(1, "Same institution is enrolled and participates", "A1", 1),
+  "INSERT_INTO" + ACTIVITY_COLUMNS + generateAssessmentActivityTuple(2, "Same institution is enrolled and participates", "A2", 1),
+  "INSERT_INTO" + ACTIVITY_COLUMNS + generateAssessmentActivityTuple(3, "Same institution is enrolled and does not participate", "A3", 1),
+  "INSERT_INTO" + ACTIVITY_COLUMNS + generateAssessmentActivityTuple(4, "Same institution is not enrolled", "A4", 1),
+  "INSERT_INTO" + ACTIVITY_COLUMNS + generateAssessmentActivityTuple(5, "Same institution before end date", "A5", 1),
+  "INSERT_INTO" + ACTIVITY_COLUMNS + generateAssessmentActivityTuple(6, "Other institution is enrolled and participates", "A6", 2),
+];
+
+const enrollmentQueries = [
+  "INSERT_INTO" + ENROLLMENT_COLUMNS + generateAssessmentEnrollmentTuple(1, 1, 3 ),
+  "INSERT_INTO" + ENROLLMENT_COLUMNS + generateAssessmentEnrollmentTuple(2, 2, 3 ),
+  "INSERT_INTO" + ENROLLMENT_COLUMNS + generateAssessmentEnrollmentTuple(3, 3, 3 ),
+  "INSERT_INTO" + ENROLLMENT_COLUMNS + generateAssessmentEnrollmentTuple(4, 6, 3 ),
+];
+
+const participationQueries = [
+    "INSERT_INTO" + PARTICIPATION_COLUMNS + generateAssessmentParticipationTuple(1, 1 , 3),
+    "INSERT_INTO" + PARTICIPATION_COLUMNS + generateAssessmentParticipationTuple(2, 2 , 3),
+    "INSERT_INTO" + PARTICIPATION_COLUMNS + generateAssessmentParticipationTuple(3, 6 , 3),
+];
 
 Cypress.Commands.add('deleteAllButArs', () => {
   cy.task('queryDatabase', {
@@ -62,6 +87,46 @@ Cypress.Commands.add('createDemoEntities', () => {
   })
 });
 
+  Cypress.Commands.add('createAssessmentDemoEntities', () => {
+    cy.task('queryDatabase', {
+      query: "INSERT_INTO" + INSTITUTION_COLUMNS + generateInstitutionTuple(1),
+      credentials: credentials,
+    })
+    cy.task('queryDatabase', {
+      query: "INSERT_INTO" + INSTITUTION_COLUMNS + generateOtherInstitutionTuple(2),
+      credentials: credentials,
+    })
+    cy.task('queryDatabase',  {
+      query: "INSERT INTO " + USER_COLUMNS + generateUserTuple(2, "MEMBER","DEMO-MEMBER", "MEMBER", 1),
+      credentials: credentials,
+    })
+    cy.task('queryDatabase',  {
+      query: "INSERT INTO " + AUTH_USERS_COLUMNS + generateAuthUserTuple(2, "DEMO", "demo-member", 2),
+      credentials: credentials,
+    })
+    cy.task('queryDatabase',  {
+      query: "INSERT INTO " + USER_COLUMNS + generateUserTuple(3, "VOLUNTEER","DEMO-VOLUNTEER", "VOLUNTEER", "NULL"),
+      credentials: credentials,
+    })
+    cy.task('queryDatabase',  {
+      query: "INSERT INTO " + AUTH_USERS_COLUMNS + generateAuthVolunteerTuple(2, "DEMO", "demo-volunteer", 2),
+      credentials: credentials,
+    })
+    cy.task('queryDatabase', {
+      query: activityQueries,
+      credentials: credentials,
+    })
+    cy.task('queryDatabase', {
+      query: enrollmentQueries,
+      credentials: credentials,
+    })
+    cy.task('queryDatabase', {
+      query: participationQueries,
+      credentials: credentials,
+    })
+
+  })
+
 function generateAuthUserTuple(id, authType, username, userId) {
   return "VALUES ('"
     + authType + "', '"
@@ -83,3 +148,33 @@ function generateInstitutionTuple(id) {
   return "VALUES ('"
     + id + "', 't', 'abca428c09862e89', '2022-08-06 17:58:21.402146','demo_institution@mail.com', 'DEMO INSTITUTION', '000000000', '2024-02-06 17:58:21.402134')";
 }
+
+function generateOtherInstitutionTuple(id) {
+  return "VALUES ('"
+      + id + "', 't', 'abca428c09862e89', '2022-08-06 17:58:21.402146','demo_institution@mail.com', 'DEMO INSTITUTION-2', '000000002', '2024-02-06 17:58:21.402134')";
+}
+
+function generateAuthVolunteerTuple(id, authType, username, userId) {
+  return "VALUES ('"
+      + authType + "', '"
+      + id + "', 't', 'demo_volunteer@mail.com','"
+      + username + "', '"
+      + userId + "')"
+}
+
+function generateAssessmentActivityTuple(id, description, name, institutionId) {
+    return "VALUES ('"
+        + id + "', '2024-02-06 17:58:21.402146', '2024-02-06 17:58:21.402146'" + description
+        + "', '2024-02-08 10:58:21.402146'" + name + "', 'Lisbon', '2024-02-07 17:58:21.402146', 'APPROVED'" + institutionId + ")";
+}
+
+function generateAssessmentEnrollmentTuple(id, activityId, volunteerId) {
+    return "VALUES ('"
+        + id + "', '2024-02-06 18:51:37.595713', 'sql-inserted-motivation'" + activityId + volunteerId + ")";
+  }
+
+function generateAssessmentParticipationTuple(id, activityId, volunteerId) {
+    return "VALUES ('"
+  + id + "', '2024-02-06 18:51:37.595713', '5'" + activityId + volunteerId + ")";
+}
+
