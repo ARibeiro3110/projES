8974e89e698a8c32ffb11ab633c10890bfd37212
Miguel Monte e Freitas <miguelmontefreitas@tecnico.ulisboa.pt>
2024-03-27 14:15:31 +0000

    test(Enrolment): implement end-to-end test as a Member after enrolment

--- frontend/tests/e2e/specs/enrollment/enrollment.js
+++ frontend/tests/e2e/specs/enrollment/enrollment.js
@@ -27,7 +27,7 @@ describe('Activity', () => {
             // check if the first activity has 0 applications
             .eq(0)
             .children()
-            .eq(3)
+            .eq(3) // applications column
             .should('contain', '0')
 
         cy.logout();
@@ -37,7 +37,41 @@ describe('Activity', () => {
         cy.logout();
 
         cy.demoAdminLogin();
-        // TODO: implement e2e test as admin
+        // intercept get institutions
+        cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
+        // intercept get enrollments
+        cy.intercept('GET', '/activities/*/enrollments').as('getEnrollments');
+
+        // go to activities list
+        cy.get('[data-cy="institution"]').click();
+
+        cy.get('[data-cy="activities"]').click();
+        cy.wait('@getInstitutions');
+
+        // check if the first activity has 1 application
+        cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+            .eq(0)
+            .children()
+            .eq(3)
+            .should('contain', '1')
+        
+        // select Show Applications for the first activity
+        cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+            .eq(0)
+            .find('[data-cy="showEnrollments"]')
+            .click();
+        
+        cy.wait('@getEnrollments');
+        
+        cy.get('[data-cy="enrollmentTable"] tbody tr')
+            // check if there is 1 application
+            .should('have.length', 1)
+            // check if the first application has the correct motivation
+            .eq(0)
+            .children()
+            .eq(0) // motivation column
+            .should('contain', MOTIVATION);
+
         cy.logout();
     });
 });
