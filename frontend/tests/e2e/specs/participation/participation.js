c40105a9e6f1057b7ebf48dc4f6606fbf5c17bf5
Afonso Ribeiro <afonsodaconceicaoribeiro@tecnico.ulisboa.pt>
2024-03-27 23:27:01 +0000

    test: Implement end-to-end Test for Enrollment Table Verifications
    Refs: #70

--- frontend/tests/e2e/specs/participation/participation.js
+++ frontend/tests/e2e/specs/participation/participation.js
@@ -27,24 +27,32 @@ describe('Participation', () => {
       cy.get('[data-cy="memberActivitiesTable"] tbody tr')
           .eq(0).children().eq(3).should('contain', 1);
 
-      // TODO: implement enrollment table verifications
-      
-      // // create a participation for the first enrollment
-      // cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
-      //   .first().find('[data-cy="selectParticipantButton"]').click();
-      // cy.get('[data-cy="ratingInput"]').type(RATING);
-      // cy.get('[data-cy="makeParticipantButton"]').click();
-      // cy.wait('@participate');
+      // check first activity has 2 enrollments
+      cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+        .first().find('[data-cy="showEnrollments"]').click();
+      cy.wait('@getEnrollments');
+      cy.get('[data-cy="activityEnrollmentsTable"] tbody tr').should('have.length', 2);
 
-      // // verify that the first enrollment of the table has participating as true
-      // cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
-      //     .eq(0).children().eq(2).should('contain', true);
+      // check first enrollment has participating as false
+      cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
+      .eq(0).children().eq(2).should('contain', false);
+      
+      // create a participation for the first enrollment
+      cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
+        .first().find('[data-cy="selectParticipantButton"]').click();
+      cy.get('[data-cy="ratingInput"]').type(RATING);
+      cy.get('[data-cy="makeParticipantButton"]').click();
+      cy.wait('@participate');
+
+      // verify that the first enrollment of the table has participating as true
+      cy.get('[data-cy="activityEnrollmentsTable"] tbody tr')
+          .eq(0).children().eq(2).should('contain', true);
         
-      // // verify that the first activity has 2 participations
-      // cy.get('[data-cy="getActivities"]').click();
-      // cy.wait('@getInstitutions');
-      // cy.get('[data-cy="memberActivitiesTable"] tbody tr')
-      //     .eq(0).children().eq(3).should('contain', 2);
+      // verify that the first activity has 2 participations
+      cy.get('[data-cy="getActivities"]').click();
+      cy.wait('@getInstitutions');
+      cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+          .eq(0).children().eq(3).should('contain', 2);
 
       cy.logout();
   });
