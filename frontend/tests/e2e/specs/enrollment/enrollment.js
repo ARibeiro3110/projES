faacf773f057990422848ba233bdbc7ab9218d27
Miguel Monte e Freitas <miguelmontefreitas@tecnico.ulisboa.pt>
2024-03-27 02:56:27 +0000

    test(Enrolment): implement end-to-end test as a Member before enrolment

--- frontend/tests/e2e/specs/enrollment/enrollment.js
+++ frontend/tests/e2e/specs/enrollment/enrollment.js
@@ -12,7 +12,24 @@ describe('Activity', () => {
         const MOTIVATION = 'I want to make a difference';
 
         cy.demoMemberLogin()
-        // TODO: implement e2e test as member
+        // intercept get institutions
+        cy.intercept('GET', '/users/*/getInstitution').as('getInstitutions');
+        
+        // go to activities list
+        cy.get('[data-cy="institution"]').click();
+
+        cy.get('[data-cy="activities"]').click();
+        cy.wait('@getInstitutions');
+
+        cy.get('[data-cy="memberActivitiesTable"] tbody tr')
+            // check if there are 3 activities
+            .should('have.length', 3)
+            // check if the first activity has 0 applications
+            .eq(0)
+            .children()
+            .eq(3)
+            .should('contain', '0')
+
         cy.logout();
 
         cy.demoVolunteerLogin();
